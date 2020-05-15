65b17e52e644293e357d4b9f5762924e393ddaf5
Tim Scully <hello@timscully.co.uk>
2020-05-15 13:39:51 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # The Dicee Game
+
+### Roll the dice!
+### Find out who wins by clicking below
+
+[![love-calculator](https://timscully-github.github.io/the-dicee-game/images/preview.png)](https://timscully-github.github.io/the-dicee-game/)
+
+##### The idea of the task was to create 2 dice that generate a random number and show a result message to which dice outcome was higher or whether it it was a draw.
+
+#### Technologies used
+* HTML5
+* CSS3
+* JavaScript
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var randomNumber1 = Math.floor((Math.random() * 6) + 1);
-var randomNumber2 = Math.floor((Math.random() * 6) + 1);
-var changeLeftImg = (document.getElementById("img1").src = "images/dice" + randomNumber1 + ".png");
-var changeRightImg = (document.getElementById("img2").src = "images/dice" + randomNumber2 + ".png");
+function dicee() {
+    var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+    var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+    document.getElementById("img1").src = "images/dice" + randomNumber1 + ".png";
+    document.getElementById("img2").src = "images/dice" + randomNumber2 + ".png";
 
-if (randomNumber1 > randomNumber2) {
-    document.getElementById("winner").innerHTML = "Player 1 wins!";
-} else if (randomNumber1 < randomNumber2) {
-    document.getElementById("winner").innerHTML = "Player 2 wins!";
-} else {
-    document.getElementById("winner").innerHTML = "It's a draw!";
+    if (randomNumber1 > randomNumber2) {
+        document.getElementById("winner").innerHTML = "Player 1 wins! 🎉";
+    } else if (randomNumber1 < randomNumber2) {
+        document.getElementById("winner").innerHTML = "Player 2 wins! 🎉";
+    } else {
+        document.getElementById("winner").innerHTML = "It's a draw!";
+    }
 }
+
+dicee();
