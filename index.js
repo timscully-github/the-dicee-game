307fd78a22ddcf30df24c7f22d0b18df02e0c2f5
Tim Scully <hello@timscully.co.uk>
2020-05-15 13:14:24 +0100

    created variable to update image source randomly

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var randomNumber1;
-
-randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var changeLeftImg = (document.getElementById("img1").src = "images/dice" + randomNumber1 + ".png");
 
 console.log(randomNumber1);
