5952100e0eb294cfe85373e82f95bc27a7be5ead
Tim Scully <hello@timscully.co.uk>
2020-05-15 13:20:14 +0100

    updated img 2 to reflect img 1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 var changeLeftImg = (document.getElementById("img1").src = "images/dice" + randomNumber1 + ".png");
-
-console.log(randomNumber1);
+var changeRightImg = (document.getElementById("img2").src = "images/dice" + randomNumber2 + ".png");
